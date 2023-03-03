1bd66d3529e8efd21c045f73e336f6d46fbb2c7d
meencodewriter <meencodewriter@gmail.com>
2023-03-03 10:28:27 +0900

    공지사항 작성기능 구현 완
    아직 sesstion에 signedMember 객체가 없어 추후 수정 필요

--- barogaebalochaSemiProject/src/main/java/semi/team/baro/notice/model/dao/NoticeDao.java
+++ barogaebalochaSemiProject/src/main/java/semi/team/baro/notice/model/dao/NoticeDao.java
@@ -1,14 +1,32 @@
 package semi.team.baro.notice.model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import common.JDBCTemplate;
 import semi.team.baro.notice.model.vo.Notice;
 
 public class NoticeDao {
 
 	public int insertNotice(Connection connection, Notice notice) {
-		// TODO Auto-generated method stub
-		return 0;
+		PreparedStatement preparedStatement = null;
+		String query = "insert into notice values(notice_seq.nextval,?,?,?,?,0,(TO_CHAR(SYSDATE,'YYYY-MM-DD/HH24:MI:SS')))";
+		int result = 0;
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, notice.getMemberNo());
+			preparedStatement.setString(2, notice.getNoticeCategory());
+			preparedStatement.setString(3, notice.getNoticeTitle());
+			preparedStatement.setString(4, notice.getNoticeContent());
+			result = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(preparedStatement);
+		}
+		return result;
 	}
 
 }
