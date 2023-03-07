67d2f9945b0459ccc37ed5574e82775d5a1ab62a
tomhwajung <hwajung135@naver.com>
2023-03-07 20:43:02 +0900

    blacklistsrvice 하는중

--- barogaebalochaSemiProject/src/main/java/semi/team/baro/blacklist/model/dao/BlacklistDao.java
+++ barogaebalochaSemiProject/src/main/java/semi/team/baro/blacklist/model/dao/BlacklistDao.java
@@ -1,5 +1,10 @@
 package semi.team.baro.blacklist.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BlacklistDao {
 
 }
--- barogaebalochaSemiProject/src/main/java/semi/team/baro/blacklist/model/service/BlacklistService.java
+++ barogaebalochaSemiProject/src/main/java/semi/team/baro/blacklist/model/service/BlacklistService.java
@@ -1,6 +1,12 @@
 package semi.team.baro.blacklist.model.service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import common.JDBCTemplate;
 import semi.team.baro.blacklist.model.dao.BlacklistDao;
+import semi.team.baro.blacklist.model.vo.Blacklist;
+import semi.team.baro.blacklist.model.vo.BlacklistPageData;
 
 public class BlacklistService {
 	private BlacklistDao dao;
@@ -9,5 +15,11 @@ public class BlacklistService {
 		super();
 		dao = new BlacklistDao();
 	}
+
+	public BlacklistPageData adminBlacklistList(int reqPage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
 }
