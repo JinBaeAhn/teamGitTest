9a6143896ff5cc1416760d262b812b21d14707bd
tomhwajung <hwajung135@naver.com>
2023-03-07 17:44:56 +0900

    adminsearchid 작업

--- barogaebalochaSemiProject/src/main/java/semi/team/baro/member/model/dao/MemberDao.java
+++ barogaebalochaSemiProject/src/main/java/semi/team/baro/member/model/dao/MemberDao.java
@@ -313,6 +313,39 @@ public class MemberDao {
 	}
 
 
+	public ArrayList<Member> adminselectAllMember(Connection conn, String memberId) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<Member> list = new ArrayList<Member>();
+		String query = "select * from member_tbl where member_id=?";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberId);
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				Member m = new Member();
+				m.setMemberNo(rset.getInt("member_no"));
+				m.setMemberId(rset.getString("member_id"));
+				m.setMemberPw(rset.getString("member_pw"));
+				m.setMemberName(rset.getString("member_name"));
+				m.setMemberMail(rset.getString("member_mail"));
+				m.setMemberPhone(rset.getString("member_phone"));
+				m.setMemberAddr(rset.getString("member_addr"));
+				m.setMemberLevel(rset.getInt("member_level"));
+				m.setEnrollDate(rset.getString("enroll_date"));
+				m.setMemberCredit(rset.getInt("member_credit"));
+				list.add(m);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+		return list;
+	}
 
 	
 }
